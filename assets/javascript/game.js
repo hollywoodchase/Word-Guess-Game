80060100fc6caced5303f3d6553da4121f617c44
Danny Calise <dannyjcalise@gmail.com>
2019-08-19 21:30:06 -0400

    finished

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,6 +3,8 @@ var chosenWord = words[Math.floor(Math.random()*words.length)];
 var dashedWord = [];
 var livesCount = 5;
 var winCount = 0;
+var wrongLetters = "";
+var correctCount = 0;
 
 splitWord = chosenWord.split('');
 for (let i = 0; i < splitWord.length; i++) {
@@ -17,13 +19,17 @@ lives.innerHTML="<h3>Lives remaining: </h3>" + livesCount;
 var gameZone = document.getElementById("game-zone");
 var nickSad = document.getElementById('loss-screen');
 var winScreen = document.getElementById('win-screen');
-var guessedLetters = document.getElementById('guessed-letters');
+var letters = document.getElementById('letters');
+var goodAudio = document.getElementById('good-audio');
+var badAudio = document.getElementById('bad-audio');
+var correctGuesses = document.getElementById('correct-guesses');
 
 document.onkeyup = function(event) {
     var userInput = event.key;
     currentLetter.innerHTML = "<h3>Letter you pushed: <br>" + userInput + "</h3>";
     for (let j = 0, noCount = 0, dashCount = 0; j < splitWord.length; j++) {
         if (userInput === splitWord[j]) {
+            goodAudio.play();
             dashedWord=dashedWord.split('');
             dashedWord[j]=splitWord[j];
             if (j===0) {
@@ -31,14 +37,18 @@ document.onkeyup = function(event) {
             }
             dashedWord=dashedWord.join('');
             dashes.innerHTML=dashedWord;
+            correctCount++;
+            correctGuesses.innerHTML="<h3 id='correct-guesses'>Correct guesses: " + correctCount + "</h3>";
         } 
         else {
             noCount++;
         }
         if (noCount === dashedWord.length) {
+            badAudio.play();
             livesCount--;
             lives.innerHTML="<h3>Lives remaining: </h3>" + livesCount;
-            guessedLetters.innerHTML = "<h3 class='text'>Letters Already Guessed</h3><br><h4>" + userInput;
+            wrongLetters+=(" " + userInput);
+            letters.innerHTML = wrongLetters;
         }
         dashedWord=dashedWord.split('');
         for (let k = 0, noDash = 0; k < dashedWord.length; k++) {
@@ -48,16 +58,21 @@ document.onkeyup = function(event) {
             if (noDash === dashedWord.length) {
                 gameZone.classList.add('game-over');
                 winScreen.classList.remove('game-over');
-
+                document.onkeyup = function() {
+                    location.reload();
+                } 
             }
         }
         dashedWord=dashedWord.join('');
-        
+        if (livesCount === 0) {
+            gameZone.classList.add('game-over');
+            nickSad.classList.remove('game-over');
+            document.onkeyup = function() {
+                location.reload();
+            } 
+        }
     }
-    if (livesCount === 0) {
-        gameZone.classList.add('game-over');
-        nickSad.classList.remove('game-over');
-    }
+    
 
 }
 
